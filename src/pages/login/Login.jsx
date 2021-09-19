a7cf302f06754c584c76926a49fe1eb46abd74fd
theshifaligwal <gwal.shifali2223@gmail.com>
2021-09-19 11:41:03 +0530

    Import material ui

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -2,7 +2,7 @@
 import "./login.css";
 import { loginCall } from "../../apiCalls";
 import { AuthContext } from "../../context/AuthContext";
-// import { CircularProgress } from "@material-ui/core";
+ import { CircularProgress } from "@material-ui/core";
 
 export default function Login() {
   const email = useRef();
