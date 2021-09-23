8af83457cc0514465929596f3bb30e772e39fd54
theshifaligwal <gwal.shifali2223@gmail.com>
2021-09-23 13:45:59 +0530

    Link api

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,11 +1,16 @@
 import axios from "axios";
 
 export const loginCall = async (userCredential, dispatch) => {
+  
+  const { REACT_APP_BACKEND_API_LINK } = process.env;
   dispatch({ type: "LOGIN_START" });
   try {
-    const res = await axios.post("https://inbound-media.herokuapp.com/api/auth/login", userCredential);
+    const res = await axios.post(
+      `${REACT_APP_BACKEND_API_LINK}/auth/login`,
+      userCredential
+    );
     dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
   } catch (err) {
     dispatch({ type: "LOGIN_FAILURE", payload: err });
   }
-};
+}; 
